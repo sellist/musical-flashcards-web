d54ff51f1916a06a4056c21ebb473eb4846cf13d
Scott Ellis <scottellis94@gmail.com>
2024-04-15 23:12:31 -0400

    add react buddy dev support

--- src/main.jsx
+++ src/main.jsx
@@ -5,30 +5,36 @@ import './index.css'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Contact from "./routes/Contact.jsx";
 import About from "./routes/About.jsx";
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev/index.js";
 
 const router = createBrowserRouter(
-[
-    {
-        path: '/',
-        element: <App/>,
-    },
-    {
-        path: '/contact',
-        element: Contact,
-    },
-    {
-        path: '/about',
-        element: About,
-    },
-    {
-        path: '/deck/',
-        element: <h1>Deck</h1>,
-    }
-]
+    [
+        {
+            path: '/',
+            element: <App/>,
+        },
+        {
+            path: '/contact',
+            element: Contact,
+        },
+        {
+            path: '/about',
+            element: About,
+        },
+        {
+            path: '/deck/',
+            element: <h1>Deck</h1>,
+        }
+    ]
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <DevSupport ComponentPreviews={ComponentPreviews}
+                    useInitialHook={useInitial}
+        >
+            <RouterProvider router={router}/>
+        </DevSupport>
     </React.StrictMode>,
 )
